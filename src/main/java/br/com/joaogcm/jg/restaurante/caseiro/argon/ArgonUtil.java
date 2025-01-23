edad041a2efc8fa93882ee1d410081de721c0094
Joao Gabriel <joaogabriel36@hotmail.com>
2025-01-23 18:13:22 -0300

    Liberando acesso as telas principais da aplicação

--- src/main/java/br/com/joaogcm/jg/restaurante/caseiro/argon/ArgonUtil.java
+++ src/main/java/br/com/joaogcm/jg/restaurante/caseiro/argon/ArgonUtil.java
@@ -5,6 +5,34 @@ import de.mkammerer.argon2.Argon2Factory;
 
 public class ArgonUtil {
 	/**
+	 * Iterações: 6 é o número de iterações que o Argon2 vai fazer (Quanto maior o
+	 * número, mais difícil será para alguém atacar e mais lento fica a execução).
+	 * 
+	 * Memória: 131072 é o tamanho da memória que o Argon2 deve utilizar (Quanto
+	 * maior a memória, mais difícil será para alguém atacar utilizando GPU's e mais
+	 * lento fica a execução).
+	 * 
+	 * Threads: 1 é o número de Threads que o Argon2 vai utilizar (1 já é o
+	 * suficiente para a maioria dos casos, mas se o servidor utilizar vários
+	 * núcleos pode aumentar esse número.
+	 * 
+	 * @param senhaInserida
+	 * @return
+	 */
+	public String gerarSenhaHash(String senhaInserida) {
+		Argon2 gerarSenhaHash = Argon2Factory.create();
+
+		char[] caracteresDaSenha = senhaInserida.toCharArray();
+
+		try {
+			return gerarSenhaHash.hash(6, 131072, 1, caracteresDaSenha);
+		} finally {
+			// Limpeza do array de caracteres da senha
+			java.util.Arrays.fill(caracteresDaSenha, ' ');
+		}
+	}
+
+	/**
 	 * Verifica a senha hash armazenada no banco de dados com a senha inserida pelo
 	 * usuário na autenticação.
 	 * 
--- src/main/java/br/com/joaogcm/jg/restaurante/caseiro/filter/AuthenticationFilter.java
+++ src/main/java/br/com/joaogcm/jg/restaurante/caseiro/filter/AuthenticationFilter.java
@@ -1,10 +1,13 @@
 package br.com.joaogcm.jg.restaurante.caseiro.filter;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -36,10 +39,38 @@ public class AuthenticationFilter extends HttpFilter implements Filter {
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
 
+		String contextPath = req.getContextPath();
+
+		String[] publicUrls = { contextPath + "/", contextPath + "/index.jsp", contextPath + "/error.jsp",
+				contextPath + "/Home", contextPath + "/Autenticacao", contextPath + "/Cliente",
+				contextPath + "Lanche" };
+
+		String requestURI = req.getRequestURI();
+		String queryString = req.getQueryString();
+
+		for (String url : publicUrls) {
+			if (requestURI.equals(url)) {
+				chain.doFilter(request, response);
+				return;
+			}
+
+			if (requestURI.startsWith(url)) {
+				if (liberarAcessoParaDeterminadasPaginas(contextPath, requestURI, queryString)) {
+					chain.doFilter(request, response);
+					return;
+				} else {
+					redirecionarParaPagina(req, res, "/paginas/autenticacao/autenticar-login.jsp",
+							"Acesso negado! Faça o login ou cadastre-se!", "perigo");
+					return;
+				}
+			}
+		}
+
 		String authHeader = req.getHeader("Authorization");
 
 		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-			res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token não encontrado!");
+			redirecionarParaPagina(req, res, "/paginas/autenticacao/autenticar-login.jsp",
+					"Token inválido, realize o login!", "perigo");
 			return;
 		}
 
@@ -52,18 +83,22 @@ public class AuthenticationFilter extends HttpFilter implements Filter {
 			// Verifica o algoritmo do token
 			JWSAlgorithm jwsAlgorithm = jwsHeader.getAlgorithm();
 			if (!jwsAlgorithm.equals(JWSAlgorithm.HS512)) {
-				res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Algoritmo inválido!");
+				redirecionarParaPagina(req, res, "/paginas/autenticacao/autenticar-login.jsp",
+						"Algoritmo inválido, faça o login novamente!", "perigo");
 				return;
 			}
 
 			// Verifica a assinatura do token
 			MACVerifier macVerifier = new MACVerifier(JWTUtil.obterChaveSecretaToken().getBytes());
 			if (!signedJwt.verify(macVerifier)) {
-				res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Assinatura do token inválida!");
+				redirecionarParaPagina(req, res, "/paginas/autenticacao/autenticar-login.jsp",
+						"Assinatura do token inválida, faça o login novamente!", "perigo");
 				return;
 			}
+
 		} catch (Exception e) {
-			res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token inválido!");
+			redirecionarParaPagina(req, res, "/paginas/autenticacao/autenticar-login.jsp", "Token inválido!", "erro");
+			return;
 		}
 
 		// Se o token for válido, segue o fluxo normalmente.
@@ -72,5 +107,56 @@ public class AuthenticationFilter extends HttpFilter implements Filter {
 
 	public void init(FilterConfig fConfig) throws ServletException {
 
+	}
+
+	private void redirecionarParaPagina(HttpServletRequest request, HttpServletResponse response, String pagina,
+			String mensagem, String tipoMensagem) throws IOException, ServletException {
+		if (mensagem != null) {
+			request.setAttribute("mensagem", mensagem);
+		}
+
+		if (tipoMensagem != null) {
+			request.setAttribute("tipoMensagem", tipoMensagem);
+		}
+
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher(pagina);
+		requestDispatcher.forward(request, response);
+	}
+
+	private boolean liberarAcessoParaDeterminadasPaginas(String contextPath, String requestURI, String queryString) {
+		Map<String, String> params = getQueryParams(queryString);
+
+		if (queryString == null) {
+			return false;
+		}
+
+		if (requestURI.equals(contextPath + "/Home") && "home".equals(params.get("acao"))) {
+			return true;
+		} else if (requestURI.equals(contextPath + "/Cliente") && "cadastrarCliente".equals(params.get("acao"))) {
+			return true;
+		} else if (requestURI.equals(contextPath + "/Lanche") && "listarLanche".equals(params.get("acao"))) {
+			return true;
+		} else if (requestURI.equals(contextPath + "/Autenticacao") && "autenticarCliente".equals(params.get("acao"))) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private Map<String, String> getQueryParams(String queryString) {
+		Map<String, String> params = new HashMap<String, String>();
+
+		if (queryString != null && !queryString.isEmpty()) {
+			String[] pairs = queryString.split("&");
+
+			for (String pair : pairs) {
+				String[] keyValue = pair.split("=");
+				if (keyValue.length == 2) {
+					params.put(keyValue[0], keyValue[1]);
+				}
+			}
+		}
+
+		return params;
 	}
 }
